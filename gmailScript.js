8abb7b18ff8c50eb51c2be25f7f8d5da1faf31e6
Shamir Wehbe <shamirwehbe@me.com>
2019-12-14 13:34:04 -0500

    Update gmailScript.js

--- gmailScript.js
+++ gmailScript.js
@@ -3,8 +3,10 @@ function autoReply() {
   var date = new Date();
   var day = date.getDay();
   var hour = date.getHours();
-  var noReply = ["ls@compass.com", "feedback@compass.com", "jenkins@compass.com", "shamirwehbe@me.com"];
+  var noReply = ["ls@compass.com", "feedback@compass.com", "jenkins@compass.com", "shamirwehbe@me.com", "shamirwehbe@yahoo.com"];
   var fromEmails = [];
+  
+  
   var replyMessage = "Hello!\n\nYou have reached me during non business hours. I will respond by 9 AM next business day.\n\nIf you have any Compass.com related questions, check out Compass Academy! Learn about Compass' tools and get your questions answered at academy.compass.com.\n\nBest,\n\nShamir Wehbe";
   
   if ([6,0].indexOf(day) > -1 || (hour < 9) || (hour >= 17)) {
@@ -13,27 +15,23 @@ function autoReply() {
     var label = GmailApp.getUserLabelByName("autoReplied");
     
     for (var i = 0; i < threads.length; i++) {
-    
-      var messages = threads[i].getMessages()[0];
-      var from = messages.getFrom();
-      var email = from.substring(
-        from.lastIndexOf("<") + 1, 
-        from.lastIndexOf(">"));
+          
+      var messagesFrom = threads[i].getMessages()[0].getFrom();
+      var email = messagesFrom.substring(messagesFrom.lastIndexOf("<") + 1, messagesFrom.lastIndexOf(">"));
       fromEmails.push(email);
+                 
+      var yesReply = fromEmails.filter(function(e) {
+        return noReply.indexOf(e) ==-1;});
       
-      for (var t = 0; t < email.length; t++) {
-    
-        for (var e = 0; e < noReply.length; e++) {
-              
-          if ((threads[i].isUnread()) && (email[t] !== noReply[e])){
+      for (var e = 0; e < yesReply.length; e++) {
+      
+         if (threads[i].isUnread() && fromEmails == yesReply[e]){
             
             threads[i].reply(replyMessage);
             threads[i].markRead();
             threads[i].addLabel(label);
-      		
-          }
-      	}
-      }
+         }
+      }      	      
     }    
   } 
 }
