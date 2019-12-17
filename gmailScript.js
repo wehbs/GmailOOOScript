311ff9612b75d36299e78331ab7abdb5abe0c149
Shamir Wehbe <shamirwehbe@me.com>
2019-12-17 00:09:32 -0500

    fully working copy

--- gmailScript.js
+++ gmailScript.js
@@ -3,37 +3,28 @@ function autoReply() {
   var date = new Date();
   var day = date.getDay();
   var hour = date.getHours();
-  var noReply = ["ls@compass.com", "feedback@compass.com", "jenkins@compass.com", "shamirwehbe@me.com", "shamirwehbe@yahoo.com"];
-  var fromEmails = [];
-  var yesReply
-  
-  var replyMessage = "Hello!\n\nYou have reached me during non business hours. I will respond by 9 AM next business day.\n\nIf you have any Compass.com related questions, check out Compass Academy! Learn about Compass' tools and get your questions answered at academy.compass.com.\n\nBest,\n\nShamir Wehbe";
+  var noReply = ["ls@compass.com",
+                 "rl@compass.com",
+                 "robert@compass.com",
+                 "feedback@compass.com",
+                 "drew@compass.com",
+                 "melanie.lowenberg@compass.com"];
+  var replyMessage = "Hello!\n\nYou have reached me during non-business hours. I will respond by 9 AM next business day.\n\nIf you have any Compass.com related questions, check out Compass Academy! Learn about Compass' tools and get your questions answered at academy.compass.com.\n\nBest,\n\nShamir Wehbe";
   
   if ([6,0].indexOf(day) > -1 || (hour < 9) || (hour >= 17)) {
-    var timeFrom = Math.floor(date.valueOf()/1000) - 60 * interval;  
-    var threads = GmailApp.search('is:inbox after:' + timeFrom);
+    var timeFrom = Math.floor(date.valueOf()/1000) - 60 * interval;
+    var threads = GmailApp.search('from:@compass.com is:inbox after:' + timeFrom);
     var label = GmailApp.getUserLabelByName("autoReplied");
     
     for (var i = 0; i < threads.length; i++) {
           
       var messagesFrom = threads[i].getMessages()[0].getFrom();
       var email = messagesFrom.substring(messagesFrom.lastIndexOf("<") + 1, messagesFrom.lastIndexOf(">"));
-      fromEmails.push(email);
-            
-      yesReply = fromEmails.filter(function(e) {
-        return noReply.indexOf(e) ==-1;});
           
-      
-        for (var y = 0; y < fromEmails.length; y++) {
-        
-         if (threads[i].isUnread() && yesReply.indexOf(fromEmails[y]) > -1){
-            
+         if (threads[i].isUnread() && noReply.indexOf(email) == -1){            
             threads[i].reply(replyMessage);
-            threads[i].markRead();
             threads[i].addLabel(label);
-         
-        }
-      }      	      
-    }    
-  } 
+      }
+    }
+  }
 }
